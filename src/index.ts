4177725745bec2f7bd33dd878e7608b262494c19
Greg <gsolomonwebdev@gmail.com>
2020-09-09 22:37:27 -0700

    dsdasda

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,10 @@
  */
 import Discord from 'discord.js';
 import {messageCommands} from './commands';
+import express from 'express';
+import path from 'path';
+
+const app = express();
 
 if (process.env.NODE_ENV === 'development') {
   require('dotenv').config();
@@ -19,3 +23,18 @@ client.on('ready', () => {
 client.on('message', messageCommands);
 
 client.login(process.env.TOKEN);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('.'));
+
+  app.get('*', (_req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'));
+  });
+}
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Web Server listening`);
+});
+
